0c4ecaaa67cf2b0ad5ee27c3869ed9eb91cb9c82
Thomas Salemy <thomas.salemy@gmail.com>
2019-09-15 21:13:18 -0400

    added padding

--- cse475/bankers-map/src/Node.h
+++ cse475/bankers-map/src/Node.h
@@ -4,5 +4,6 @@ public:
   K key;
   volatile V val;
   Node<K, V>* next;
+  char padding[48]; //assumes key = 4bytes, val = 4 bytes, next = 8bytes;
   Node(K key, V val): key(key), val(val), next(NULL) {}
 };
